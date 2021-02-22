e70d52f8b7c47509d98bd03da378bc5c292ab7f7
Pravin Patel <Pravin.Patel@phoenix.edu>
2021-02-21 17:55:12 -0700

    Added typescript variable

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -28,7 +29,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-  app.get( "/filteredimage/",  ( req, res ) => {
+  app.get( "/filteredimage/",  ( req: Request, res: Response) => {
 
     let { image_url } = req.query;
     if ( !image_url ) {
